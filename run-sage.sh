97a7c65c138d93bc1593871598af567b2bcd5c39
john <john@jonesbook.(none)>
2010-03-04 19:00:02 +1000

    Thu Mar  4 19:00:02 VLAT 2010

--- run-sage.sh
+++ run-sage.sh
@@ -5,4 +5,4 @@ export DOT_SAGE=~/.sage/
 export PATH=$SAGE_ROOT/local/bin:$PATH
 export LD_LIBRARY_PATH="$SAGE_ROOT/local/lib/openmpi:$SAGE_ROOT/local/lib/:"
 export PYTHONPATH="$SAGE_ROOT/local/bin:$SAGE_ROOT/local/lib/python2.6/site-packages/setuptools-0.6c8-py2.6.egg:$SAGE_ROOT/local/lib/python2.6/site-packages/SQLAlchemy-0.4.3-py2.6.egg:$SAGE_ROOT/local/bin:$SAGE_ROOT/local/lib/python:$SAGE_ROOT/local/lib/python25.zip:$SAGE_ROOT/local/lib/python2.6/plat-linux2:$SAGE_ROOT/local/lib/python2.6/lib-tk:$SAGE_ROOT/local/lib/python2.6/lib-dynload:$SAGE_ROOT/local/lib/python2.6/site-packages:$SAGE_ROOT/local/lib/python2.6/site-packages/IPython/Extensions"
-ipython
+python
---
+++ s.py
@@ -0,0 +1,52 @@
+import os
+import re
+import sys
+def set_paths(root="/home/john/projects/sage-4.3.3"):
+    """
+    Set paths necessary to import sage.
+    root ... the path to your Sage installation
+    """
+    python_path = [
+        root+"/local/lib/python",
+        root+"/local/lib/python2.6/lib-dynload",
+        root+"/local/lib/python2.6/site-packages",
+            ]
+    sys.path = python_path + sys.path
+    paths = {
+            "SAGE_ROOT": root,
+            "SAGE_DOC": root +  "/devel/sage/doc",
+            "SAGE_LOCAL": root+"/local",
+            "DOT_SAGE": "~/.sage/",
+            "SAGE_SERVER": "http://www.sagemath.org/",
+            "PATH": root+"/local/bin:"+os.environ["PATH"],
+            }
+    os.environ.update(paths)
+def import_sageall():
+    """
+    Imports sage by "import sage.all".
+    If it fails, write a helpful message and exit. After executing this method,
+    you can be sure that sage just works, e.g you can do
+    from sage.all import something
+    without worrying.
+    """
+    try:
+        import sage.all
+    except ImportError, e:
+        if re.match("(.*).so: cannot open shared object file: No such file or directory", e.message):
+            # all seems to be just a LD_LIBRARY_PATH problem in 99% of cases
+            print """\
+"import sage.all" failed when importing an .so library, this is most probably
+an LD_LIBRARY_PATH problem, please set your LD_LIBRARY_PATH and try again, e.g. execute:
+LD_LIBRARY_PATH=%s/local/lib python %s""" % (os.environ.get("SAGE_ROOT", ""),
+        sys.argv[0])
+            sys.exit()
+        else:
+            # some other problem
+            raise
+set_paths()
+import_sageall()
+from sage.all import *
+var("x")
+e = x**2/3-log(x)
+print e
+print integrate(e, x)
