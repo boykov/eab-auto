45366ff9ed3c31e67a5bdb37aebca487e66bec58
john <john@jonesbook.(none)>
2010-03-26 15:31:14 +1000

    Птн Мар 26 15:31:14 VLAT 2010

--- org-git-sync.sh
+++ org-git-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Add org file changes to the repository
 # [[file:/etc/crontab][crontab]]
-REPOS="org cc mpl hron home emacs auto" 
+REPOS="org cc mpl hron home emacs auto timekeeper" 
 
 for REPO in $REPOS
 do
