d0c8d2a7a6eb56c21fd3209ea621810547216965
Evgeny Boykov <john@jonesbook.(none)>
2011-05-11 15:00:14 +1100

    Wed May 11 15:00:14 VLAST 2011

--- org-git-sync.sh
+++ org-git-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Add org file changes to the repository
 # [[file:/etc/crontab][crontab]]
-REPOS="org cc mpl hron home emacs auto python timekeeper difwave/uu difwave/upr_upr difwave/forgen difwave/gmres" 
+REPOS="org cc mpl hron home emacs auto python timekeeper difwave/uu difwave/upr_upr difwave/forgen difwave/gmres difwave/namelib" 
 
 for REPO in $REPOS
 do
