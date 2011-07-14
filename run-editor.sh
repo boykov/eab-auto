3a0c1500ddad4e6085b918465a8af06d010d0a07
Евгений Бойков <eab@victory.(none)>
2011-07-14 20:39:38 +1100

    Чт. июля 14 20:39:38 VLAST 2011

--- run-editor.sh
+++ run-editor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # /usr/local/bin/emacs # -fn -ETL-Fixed-Medium-R-Normal--16-160-72-72-C-80-ISO8859-1
-if !(ps -e | grep emacs) # 
+if (ps -e | grep emacs) # 
 then
-    `which emacs` --daemon
+   bash -i -c `emacsclient -c`&
 else
-    bash -i -c `emacsclient -c`&
+    `which emacs` --daemon
 fi
