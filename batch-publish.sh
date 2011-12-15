3c267fab5c2350cc66c0b6f24495eb370463d358
Евгений Бойков <eab@victory.(none)>
2011-12-15 15:02:02 +1100

    Чт. дек. 15 15:02:02 VLAT 2011

--- batch-publish.sh
+++ batch-publish.sh
@@ -4,4 +4,5 @@
 
 DOTPATH=/home/eab/git/emacs/init.el
 
-/usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(org-publish-project \"html\" t)" -l /home/eab/git/emacs/eab-auto-exec.el
+/usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(org-publish-project \"html\" t)"
+# -l /home/eab/git/emacs/eab-auto-exec.el
