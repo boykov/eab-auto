064474c1a0cf13ead02058c3aa5e3fc0517fa3af
Евгений Бойков <eab@victory.(none)>
2012-01-30 20:02:01 +1100

    Пн. янв. 30 20:02:01 VLAT 2012

--- run-editor.sh
+++ run-editor.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # /usr/local/bin/emacs # -fn -ETL-Fixed-Medium-R-Normal--16-160-72-72-C-80-ISO8859-1
-if (ps -e | grep emacs) # 
-then
-   bash -i -c `emacsclient -c`&
-else
-    `which emacs` --daemon
-fi
+bash -i -c `emacsclient -c`&
+# if (ps -e | grep emacs) # 
+# then
+#    bash -i -c `emacsclient -c`&
+# else
+#     `which emacs` --daemon
+# fi
