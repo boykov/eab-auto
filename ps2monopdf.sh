1e85cfa7b1b9572a00a6d249913d6b3a16df7094
Евгений Бойков <eab@victory.(none)>
2012-02-21 20:02:02 +1100

    Вт. февр. 21 20:02:02 VLAT 2012

--- ps2monopdf.sh
+++ ps2monopdf.sh
@@ -3,3 +3,5 @@ filename=$*
 djvups $filename.djvu $filename.ps 
 gs -dSAFER -dBATCH -dNOPAUSE -r300x300 -dDITHERPPI=120 -sDEVICE=psmono -sOutputFile=tmp.ps $filename.ps
 ps2pdf tmp.ps $filename.pdf
+rm -f tmp.ps
+rm -f $filename.ps
