93cb6c749e908cf28d6850bea17a5615c1867d25
Евгений Бойков <eab@victory.(none)>
2012-02-21 19:02:02 +1100

    Вт. февр. 21 19:02:02 VLAT 2012

--- ps2monopdf.sh
+++ ps2monopdf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-
-gs -dSAFER -dBATCH -dNOPAUSE -r300x300 -dDITHERPPI=120 -sDEVICE=psmono -sOutputFile=gordeev2001sistemnoe_.ps gordeev2001sistemnoe.ps
-ps2pdf gordeev2001sistemnoe_.ps gordeev2001sistemnoe.pdf
+filename=$*
+djvups $filename.djvu $filename.ps 
+gs -dSAFER -dBATCH -dNOPAUSE -r300x300 -dDITHERPPI=120 -sDEVICE=psmono -sOutputFile=tmp.ps $filename.ps
+ps2pdf tmp.ps $filename.pdf
