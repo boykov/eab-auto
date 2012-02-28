67da56d69ce11f4d6cad95c322073931004002ca
Evgeny Boykov <artscan@list.ru>
2012-02-28 23:02:01 +1100

    Вт. февр. 28 23:02:01 VLAT 2012

--- org-git-sync.sh
+++ org-git-sync.sh
@@ -11,7 +11,7 @@ do
     git ls-files --deleted -z | xargs -0 git rm >/dev/null 2>&1
 # Add new files
     git add . >/dev/null 2>&1
-    git config --global user.name "Evgeny Boykov"
-    git config --global user.email artscan@list.ru
+    # git config --global user.name "Evgeny Boykov"
+    # git config --global user.email artscan@list.ru
     git commit -m "$(date)"
 done
