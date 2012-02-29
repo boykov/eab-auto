91a5ff4b02ef8b54670b6639656ba83c5dd4f968
Evgeny Boykov <artscan@list.ru>
2012-02-29 22:02:02 +1100

    Ср. февр. 29 22:02:02 VLAT 2012

--- batch-publish.sh
+++ batch-publish.sh
@@ -3,6 +3,10 @@
 # [[file:/etc/crontab][crontab]]
 
 DOTPATH=/home/eab/git/emacs/init.el
+export EMACS_SERVER_NAME=serverN
 
-/usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(org-publish-project \"html\" t)"
-# -l /home/eab/git/emacs/eab-auto-exec.el
+# /usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(org-publish-project \"html\" t)"
+# /usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(eab/send-csum)"
+
+emacsclient -s $EMACS_SERVER_NAME --eval "(org-publish-project \"html\" t)"
+emacsclient -s $EMACS_SERVER_NAME --eval "(eab/send-csum)"
