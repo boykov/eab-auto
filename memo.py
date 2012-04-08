099ff9f99076c03420c5e3f07948bd9eace6e27a
Evgeny Boykov <artscan@list.ru>
2012-04-08 21:02:01 +1100

    Вс. апр.  8 21:02:01 VLAT 2012

--- memo.py
+++ memo.py
@@ -3,9 +3,12 @@
 
 import functools
 import cPickle
+from ellipsoid import *
+import shelve
+from numpy import *
 
 def memoize(fctn):
-    memory = {}
+    memory = shelve.open("memo.dat")
     @functools.wraps(fctn)
     def memo(*args,**kwargs):
         haxh = cPickle.dumps((args, sorted(kwargs.iteritems())))
@@ -18,4 +21,10 @@ def memoize(fctn):
     if memo.__doc__:
         memo.__doc__ = "\n".join([memo.__doc__,"This function is memoized."])
     return memo
-        
+
+@memoize
+def wrapellipsoid(axes,points):
+    return ellipsoid(axes,points)
+
+if __name__ == "__main__":
+    e = wrapellipsoid(array([0.75,1,0.5]),200)
