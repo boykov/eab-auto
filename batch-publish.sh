c3e0c3f1eb64bde2cf00219b7c7076e62170ed47
Evgeny Boykov <artscan@list.ru>
2012-06-14 02:02:03 +1100

    Чт. июня 14 02:02:03 VLAT 2012

--- batch-publish.sh
+++ batch-publish.sh
@@ -9,9 +9,4 @@ export DISPLAY=:0.0
 # /usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(org-publish-project \"html\" t)"
 # /usr/local/bin/emacs -Q --batch -l $DOTPATH --eval "(eab/send-csum)"
 
-emacsclient -s $EMACS_SERVER_NAME -c --eval "(progn
-                                                  (eab/update-reports-nightly)
-                                                  (org-publish-project \"html\" t)
-                                                  (org-publish-project \"pdf\" t)
-                                                  (eab/send-csum)
-                                                  (delete-frame nil 'force))"
+emacsclient -s $EMACS_SERVER_NAME -c --eval "(progn (eab/update-reports-nightly) (org-publish-project \"html\" t) (org-publish-project \"pdf\" t) (eab/send-csum) (delete-frame nil 'force))"
