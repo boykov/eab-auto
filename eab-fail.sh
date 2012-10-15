15440ab0ccab7f437bca7614d1cb8f6b960e4b2e
Evgeny Boykov <artscan@list.ru>
2012-10-15 23:02:02 +1100

    Пн. окт. 15 23:02:02 VLAT 2012

--- eab-fail.sh
+++ eab-fail.sh
@@ -3,4 +3,8 @@
 cd /var/mail
 grepmail failed eab >> ~/data/mail/inbox
 grepmail -v failed eab > ~/data/mail/eab.bak
-
+sudo rm eab
+sudo cp ~/data/mail/eab.bak eab
+sudo chown eab:mail /var/mail/eab
+sudo chmod o-r /var/mail/eab
+sudo chmod g+rw /var/mail/eab
