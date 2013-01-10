22def4f615a56534952f5e25afeb4202bf2a47be
Evgeny Boykov <artscan@list.ru>
2013-01-11 05:02:01 +1100

    Пт. янв. 11 05:02:01 VLAT 2013

--- check-cc.sh
+++ check-cc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-is=`ping -c 3 192.168.0.107 | grep -c "64 bytes"`
-   # Проверяем прошли ли пинги на мой ip адрес внутри СС, дома точно такого нет 
-if !(test $is -gt "0") # если пинги не проходят
+is=`ping -c 3 172.25.0.82 | grep -c "64 bytes"`
+   # Проверяем прошли ли пинги на Ростелеком default gateway
+if (test $is -gt "0") # если пинги проходят
 then
-    echo "CC server is down"
+    echo "Rostelecom. CC server is down"
     return 1
 else
     return 0
