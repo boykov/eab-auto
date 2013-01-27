1bf5a3b80bd5ec053146dc6f94d76fbaf87da75b
Evgeny Boykov <artscan@list.ru>
2013-01-28 05:02:02 +1100

    Пн. янв. 28 05:02:02 VLAT 2013

--- eab-fail.sh
+++ eab-fail.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 cd /var/mail
+sudo cp eab ~/data/mail/eab.bak
 grepmail failed eab >> ~/data/mail/inbox
 grepmail -v failed eab > ~/data/mail/eab.bak
-sudo rm eab # TODO если нет grepmail, то можно удалить всю почту
+sudo rm eab # DONE если нет grepmail, то можно удалить всю почту
 sudo cp ~/data/mail/eab.bak eab
 sudo chown eab:mail /var/mail/eab
 sudo chmod o-r /var/mail/eab
