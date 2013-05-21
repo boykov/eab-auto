2a23fe7eec6f59eef04a65cd692da7b49756e193
Evgeny Boykov <artscan@list.ru>
2013-05-21 11:27:15 +1100

    merge gsie and dotgsie

--- nightly-tests.sh
+++ nightly-tests.sh
@@ -6,6 +6,6 @@ export PATH=$PATH:/usr/local/bin/ # path for maxima
 
 cd ~/git/difwave/gsie && make -f Makefile libjacobian.so
 
-cd ~/git/difwave/dotgsie/
+cd ~/git/difwave/gsie/dotgsie/
 
 export LD_LIBRARY_PATH=/home/eab/git/difwave/gsie/ && python -m unittest testcover
