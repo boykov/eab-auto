6e56f4e8d3ff65c6ab305f78172acabc5e6744e1
Evgeny Boykov <artscan@list.ru>
2013-06-17 05:02:02 +1100

    Пн. июня 17 05:02:02 VLAT 2013

--- download-schedule.sh
+++ download-schedule.sh
@@ -3,6 +3,8 @@
 # [[file:/etc/crontab][crontab]]
 
 cd ~/data/tor
+mv kav_rescue_10.iso kav_rescue_10.iso.bak
+rm kav_rescue_10.md5.txt
 wget http://rescuedisk.kaspersky-labs.com/rescuedisk/updatable/kav_rescue_10.iso
 wget http://rescuedisk.kaspersky-labs.com/rescuedisk/updatable/kav_rescue_10.md5.txt
 md5sum kav_rescue_10.iso
