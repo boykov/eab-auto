594a5d7f2651ee08b916614e860829c594cf7b0c
Evgeny Boykov <artscan@list.ru>
2014-04-24 05:02:02 +1100

    Чт. апр. 24 05:02:02 VLAT 2014

--- run-editor.sh
+++ run-editor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # /usr/local/bin/emacs # -fn -ETL-Fixed-Medium-R-Normal--16-160-72-72-C-80-ISO8859-1
-bash -i -c `emacsclient -c $*`&
+bash -i -c `emacsclient -c $* -F "((fullscreen . maximized))"`&
 # if (ps -e | grep emacs) # 
 # then
 #    bash -i -c `emacsclient -c`&
