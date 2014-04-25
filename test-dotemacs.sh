a91d29db1fabc225a08efc20578a7c2b59ce03ad
Evgeny Boykov <artscan@list.ru>
2014-04-26 05:02:02 +1100

    Сб. апр. 26 05:02:02 VLAT 2014

--- test-dotemacs.sh
+++ test-dotemacs.sh
@@ -4,6 +4,6 @@
 export EMACS_SERVER_NAME=serverM
 export DISPLAY=:0.0
 
-dbus-launch `which emacs` --daemon=$EMACS_SERVER_NAME
+`which emacs` --daemon=$EMACS_SERVER_NAME
 
 emacsclient -s $EMACS_SERVER_NAME -c --eval "(eab/test-dotemacs)"
