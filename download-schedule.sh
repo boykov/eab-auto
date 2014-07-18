fdda165ebd4500e4d17aeaf47652271806adbfff
Evgeny Boykov <artscan@list.ru>
2014-07-19 05:02:02 +1100

    Сб. июля 19 05:02:02 VLAT 2014

--- download-schedule.sh
+++ download-schedule.sh
@@ -2,7 +2,7 @@
 # Add org file changes to the repository
 # [[file:/etc/crontab][crontab]]
 
-cd ~/data/tor
+cd ~/newdata/admin
 test -s kav_rescue_10.iso && mv kav_rescue_10.iso kav_rescue_10.iso.bak
 rm -f kav_rescue_10.md5.txt
 wget http://rescuedisk.kaspersky-labs.com/rescuedisk/updatable/kav_rescue_10.iso
