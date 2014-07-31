ebb20d5924cf4bc93f6ff3925d1ca183eb32da91
Evgeny Boykov <artscan@list.ru>
2014-08-01 05:02:02 +1100

    Пт. авг.  1 05:02:02 VLAT 2014

--- check-cc.sh
+++ check-cc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-is=`ping -c 3 172.25.0.82 | grep -c "64 bytes"`
+is=`ping -c 3 95.70.2.211 | grep -c "64 bytes"`
    # Проверяем прошли ли пинги на Ростелеком default gateway
 if (test $is -gt "0") # если пинги проходят
 then
