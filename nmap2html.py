02f219b45b32c23c2577303bd387cb4a5a27af35
Evgeny Boykov <artscan@list.ru>
2015-07-17 00:04:51 +1000

    add svg, filter, refactoring

--- nmap2html.py
+++ nmap2html.py
@@ -4,15 +4,42 @@
 import os
 import sys
 
+import tempfile
+
+fltr = ["22", "25", "139", "143", "445", "631", "993", "3306"]
+
 a = os.popen("nmap localhost | grep open").read()
 
 a = a.split("\n")
 
-# b = map(lambda x:x.split(" ")[-1],a)[:-1]
+a = filter(lambda x: x.split("/tcp")[0] not in fltr, a)
 
-a = map(lambda x: "<A HREF=\"http://192.168.0.107:" + x.split("/tcp")[0] + "\">"  + x.split("/tcp")[0] + " " + x.split(" ")[-1] +"</A><br>",a)
+targets = map(lambda x: ["http://192.168.0.107:" + x.split("/tcp")[0],
+                         x.split("/tcp")[0],
+                         x.split(" ")[-1]],a)[:-1]
+
+a = map(lambda x: """<A HREF="%s">%s %s</A><br>""" % tuple(x),targets)
 
 print "Content-type: text/html"
 print ''
 
-map(lambda x:sys.stdout.write(x), a[:-1])
+map(lambda x:sys.stdout.write(x), a)
+
+def f(t):
+    return """"%s %s" [URL="%s", target = "_blank"];\n""" % (t[1],t[2],t[0])
+
+nmap="""
+graph G {
+""" + "".join(map(f,targets)) + """
+}
+"""
+
+dirpath = tempfile.mkdtemp(dir='/tmp')
+
+open(dirpath + "/nmap.dot",'w').write(nmap)
+
+os.system("dot -Tsvg " + dirpath + "/nmap.dot -o " + dirpath + "/nmap.svg")
+
+print open(dirpath + "/nmap.svg",'r').read()
+
+os.system("rm -rf " + dirpath)
