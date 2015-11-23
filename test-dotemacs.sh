d25dcb45742c996f4ac3540e1cf3048ea251b885
Evgeny Boykov <artscan@list.ru>
2015-11-24 08:53:05 +1000

    add sleep 1 (fix ERROR eab/test-dotemacs)

--- test-dotemacs.sh
+++ test-dotemacs.sh
@@ -6,4 +6,6 @@ export EMACS_SERVER_NAME=serverM
 
 /home/eab/data/gitno/emacs24.4/emacs-24.5/src/emacs-24.5.1 --daemon=$EMACS_SERVER_NAME
 
+sleep 1
+
 emacsclient -s $EMACS_SERVER_NAME --eval "(eab/test-dotemacs)"
