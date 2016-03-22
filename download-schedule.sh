afeba73323b0c6d7bcc9f692c686175fb540074b
Evgeny Boykov <artscan@list.ru>
2016-03-22 15:56:40 +1000

    add drweb-livedisk-900-cd

--- download-schedule.sh
+++ download-schedule.sh
@@ -9,3 +9,6 @@ wget http://rescuedisk.kaspersky-labs.com/rescuedisk/updatable/kav_rescue_10.iso
 wget http://rescuedisk.kaspersky-labs.com/rescuedisk/updatable/kav_rescue_10.md5.txt
 md5sum kav_rescue_10.iso
 cat kav_rescue_10.md5.txt
+
+test -s drweb-livedisk-900-cd.iso && mv drweb-livedisk-900-cd.iso drweb-livedisk-900-cd.iso.bak
+wget https://download.geo.drweb.com/pub/drweb/livedisk/drweb-livedisk-900-cd.iso
