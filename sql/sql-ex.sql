c23f0fcc89f9806e9ae124371b1f03314e15d9b3
Evgeny Boykov <artscan@list.ru>
2016-10-04 23:04:49 +1000

    add working changes

--- sql/sql-ex.sql
+++ sql/sql-ex.sql
@@ -1,6 +1,7 @@
 mysqladmin -u root -p create ships
 mysqladmin -u root -p drop ships
 mysql ships < ships_mysql_script.sql
+mysql computer < ~/share/computer_mysql_script.sql
 mysql ships < query
 mysqladmin -u root -p variables 
 
@@ -54,3 +55,42 @@ FROM classes
     RIGHT JOIN outcomes ON classes.class=ship OR ships.name=ship
 WHERE battle='Guadalcanal';
 -- ------------------------------------------
+
+-- 1
+
+select Product.type, avg(price) as ap from (
+select Product.model, price, Product.type from Product left join PC on Product.model = PC.model
+union all
+select Product.model, price, Product.type from Product left join Laptop on Product.model = Laptop.model
+union all
+select Product.model, price, Product.type from Product left join Printer on Product.model = Printer.model
+    ) av  right join Product on Product.model = av.model
+where Product.model in
+(select cc.model from
+	(select count(*) num, p1.model from Product p1 join Product p2 on p1.model >= p2.model group by p1.model)
+	cc where cc.num % 5 = 0) group by Product.model, Product.type
+-- 2
+
+select ship, replace(ship, substring(ship, locate(' ', ship)+1,
+	    (length(ship) - locate(' ',reverse(ship))) - locate(' ', ship)),
+	repeat('*',(length(ship) - locate(' ',reverse(ship))) - locate(' ', ship))) as rp
+from Outcomes where (locate(' ', ship) > 0 and (length(ship ) - locate(' ', reverse(ship)) - locate(' ', ship))>0)
+
+
+select ship, replace(ship, substring(ship, charindex(' ', ship)+1,
+            (len(ship)  - charindex(' ',reverse(ship))) - charindex(' ', ship)),
+        replicate('*',(len(ship ) - charindex(' ',reverse(ship))) - charindex(' ', ship)))
+from Outcomes where
+    (charindex(' ', ship) > 0 and (len(ship ) - charindex(' ', reverse(ship)) - charindex(' ', ship))>0)
+
+select ship, replace(rp, '_', ' ') from
+(select ship, concat(substring(shiprp, 1, locate('_', shiprp)) , repeat('*',(length(shiprp) - locate('_',reverse(shiprp))) - locate('_', shiprp))
+, substring(shiprp, length(shiprp) - locate('_', reverse(shiprp))+1, locate('_', reverse(shiprp)))) as rp
+from (select ship, replace(ship, ' ', '_') as shiprp from Outcomes) bb
+where (locate('_', shiprp) > 0 and (length(shiprp) - locate('_', reverse(shiprp)) - locate('_', shiprp))>0)) ff
+
+select ship, replace(rp, '_', ' ') from
+(select ship, concat(substring(shiprp, 1, charindex('_', shiprp)) , replicate('*',(datalength(shiprp) - charindex('_',reverse(shiprp))) - charindex('_', shiprp))
+, substring(shiprp, datalength(shiprp) - charindex('_', reverse(shiprp))+1, charindex('_', reverse(shiprp)))) as rp
+from (select ship, replace(ship, ' ', '_') as shiprp from Outcomes) bb
+where (charindex('_', shiprp) > 0 and (datalength(shiprp) - charindex('_', reverse(shiprp)) - charindex('_', shiprp))>0)) ff
