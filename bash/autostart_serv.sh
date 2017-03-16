aba8480dcc30129d0d7f25a7f3562a1dada16186
Evgeny Boykov <artscan@list.ru>
2017-03-16 15:40:27 +1000

    improve autostart script

--- bash/autostart_serv.sh
+++ bash/autostart_serv.sh
@@ -6,19 +6,12 @@
 bash -i -c "export USER=eab && sudo dovecot &"
 bash -i -c "export USER=eab && /home/eab/.dropbox-dist/dropboxd &"
 # bash -i -c "/home/eab/tmp/ann/git-annex.linux/git-annex assistant --autostart &"
-# bash -i -c "export DISPLAY=:0 && gnome-screensaver-command -l"
-# bash -i -c "psensor &"
 # bash -i -c "export USER=eab && sudo docker start jenkins &"
-# eval `ssh-agent -s`
-# # bash -i -c "/opt/TakeBreak/TakeBreak &"
 # # bash -i -c "python /home/eab/git/python/watch_share.py &"
-# bash -i -c "indicator-multiload &"
-# bash -i -c "skype &"
-# # bash -i -c "transmission-gtk &"
 # # bash -i -c "sudo mount -t cifs -o user=eab,password=,dir_mode=0777,file_mode=0777 //192.168.0.4/share ~/jonesbook &"
 # bash -i -c "mono ~/KeePass/KeePass.exe &"
-# bash -i -c "export USER=eab && mediatomb -e eth0 &"
 sudo mount --bind "/home/eab/downloads/pub/papers" "/home/eab/share/papers"
 sudo mount --bind "/home/eab/git/cc/" "/home/eab/share/cc/"
-# sudo mount --bind "/home/eab/newdata/Cloud@Mail.Ru/read/" "/home/eab/share/read/"
 sudo mount --bind "/home/eab/git/cc/boykov.github.io/org-html-themes/styles" "/home/eab/pub/org/css/styles"
+sudo service samba restart
+sudo service transmission-daemon start
