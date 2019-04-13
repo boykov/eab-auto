d35858ccd09dc67a51ed779e41c6997c2a668a2e
Evgeny Boykov <artscan@list.ru>
2019-04-13 13:14:01 +1000

    update emacs-docker

--- bash/emacs-docker.sh
+++
@@ -1,9 +0,0 @@
-docker run -ti --rm -v $('pwd'):/mnt/workspace \
--v /home/eab/.emacs.d:/home/emacs/.emacs.d \
--v /etc/localtime:/etc/localtime:ro \
--v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--e DISPLAY=$DISPLAY \
--e TERM=xterm-256color \
--e TZ=UA \
- --name spacemacs jare/emacs
--- bash/run-emacssudo.sh
+++ bash/run-emacssudo.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo /usr/bin/emacs22 -fn -ETL-Fixed-Medium-R-Normal--16-160-72-72-C-80-ISO8859-1
+cd /home/eab/data/gitno/github/eab-docker-emacs/
+/bin/bash emacs-docker.sh
